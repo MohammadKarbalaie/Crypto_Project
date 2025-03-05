f3e7c4270d5e8ab842d2cc6e89c5f22ce1352de8
Mohammad karbalaiemehdi <mohammadkarbalaiemehdi@outlook.com>
2025-03-05 18:25:20 +0330

    add footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-gray-200 flex items-center justify-center py-6">
+      <p className="text-gray-800 font-semibold text-lg cursor-pointer">
+        Crypto Tracking . All rights reserved
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
